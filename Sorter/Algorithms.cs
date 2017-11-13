04020bd5e79afc27d7f6ca7d25d56df670e5e02f
Atif Butt <Atif Butt>
2017-11-13 08:47:31 -0800

    Add project files.

---
+++ Sorter/Algorithms.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Diagnostics;
+using System.IO;
+
+namespace Sorter
+{
+    public class Algorithms
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        public (int[], int) BubbleSort(int[] Arr, int t)
+        {
+
+            stopWatch.Start();
+            for (int i = 0; i <= Arr.Count() - 1; i++)
+            {
+                for (int j = 0; j < Arr.Count() - 1; j++)
+                {
+                    if (Arr[j] > Arr[j+1])
+                    {
+                        int temp = Arr[j];
+                        Arr[j] = Arr[j + 1];
+                        Arr[j + 1] = temp;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            var time = stopWatch.Elapsed.Milliseconds;
+            return (Arr, time);
+        }
+
+        public (int[], int) SelectionSort(int[] arr, int t)
+        {
+            stopWatch.Reset();
+            stopWatch.Start();
+            for (int i = 0; i < arr.Count() - 1; i++)
+            {
+                int min = arr[i];
+                for (int j = i + 1; j < arr.Count(); j++)
+                {
+                    if (arr[j] < min) min = arr[j];
+                }
+
+                if (min != arr[i])
+                {
+                    int temp = arr[i];
+                    arr[i] = min;
+                    arr[i + 1] = temp;
+                }
+            }
+
+            stopWatch.Stop();
+            var time = stopWatch.Elapsed.Milliseconds;
+            return (arr, time);
+        }
+
+        public (int[], int) InsertionSort(int[] Arr, int t)
+        {
+            int CP;
+            int VI;
+
+            stopWatch.Reset();
+            stopWatch.Start();
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                VI = Arr[i];
+                CP = i;
+
+                while (CP > 0 && Arr[CP - 1] > VI)
+                {
+                    Arr[CP] = Arr[CP - 1];
+                    CP = VI;
+                    CP = CP - 1;
+                }
+
+                Arr[CP] = VI;
+            }
+
+            stopWatch.Stop();
+            var time = stopWatch.Elapsed.Milliseconds;
+            return (Arr, t);
+        }
+    }
+}
---
+++ Sorter/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Algorithms algorithm = new Algorithms();
+                Program program = new Program();
+
+                Console.WriteLine("> Sorter");
+                Console.Write("> Write 'Auto' to Auto-Generate list or 'Browse' to browse: ");
+
+                int[] list = program.listSelection();
+                (int[] l, int t) bubbleSort = algorithm.BubbleSort(list, 0);
+                (int[] l, int t) selectionSort = algorithm.SelectionSort(list, 0);
+                (int[] l, int t) insertionSort = algorithm.InsertionSort(list, 0);
+
+                Console.WriteLine($"Bubble Sort Computation Time : {bubbleSort.t} ms");
+                Console.WriteLine($"Selection Sort Computation Time : {selectionSort.t} ms");
+                Console.WriteLine($"Insertion Sort Computation Time : {insertionSort.t} ms");
+
+                foreach (int i in bubbleSort.l) File.AppendAllText("Bubble.txt", i.ToString() + " ");
+                foreach (int i in selectionSort.l) File.AppendAllText("Selection.txt", i.ToString() + " ");
+                foreach (int i in insertionSort.l) File.AppendAllText("insertion.txt", i.ToString() + " ");
+
+                Console.WriteLine("Wait...! lists are saving in text files.");
+                Console.WriteLine("Saved successfully.....");
+
+                var input = Console.ReadLine();
+                if (input == "exit") Environment.Exit(0);
+            }
+            
+        }
+        public int[] listSelection()
+        {
+            int[] list = new int[1000000];
+            var select = Console.ReadLine();
+            if (select == "Auto")
+            {
+                Console.Write("> Enter Number Limit: ");
+                int length = int.Parse(Console.ReadLine());
+
+                Console.Write("> Enter Min Value: ");
+                int MinValue = int.Parse(Console.ReadLine());
+
+                Console.Write("> Enter Max Value: ");
+                int MaxValue = int.Parse(Console.ReadLine());
+                
+                Random rand = new Random();
+                list = new int[length];
+                for (int i = 0; i < length; i++)
+                {
+                    int no = rand.Next(MinValue, MaxValue);
+                    if (!list.Contains(no)) list[i] = no;
+                }
+                foreach (int i in list) File.AppendAllText("Random.txt", i.ToString() + Environment.NewLine);
+                return list;
+            }
+
+            else if (select == "Browse")
+            {
+                Console.Write("> Enter Path of Txt File: ");
+                var path = Console.ReadLine();
+                var file = File.ReadAllLines(path);
+                list = new int[file.Count()];
+                int i = 0;
+                foreach (string s in file)
+                {
+                    list[i] = int.Parse(s);
+                    i++;
+                }
+                Console.WriteLine("> Successfully read the file");
+                return list;
+            }
+            return list;
+        }
+    }
+}
